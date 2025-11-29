2471273662da9882e3e0f3e56673bc49a713648d
Muhammad Sohaib <msohaib.se@gmail.com>
2025-11-29 20:35:52 +0500

    AASIST with MAMBA

--- models/AASIST.py
+++ models/AASIST.py
@@ -7,6 +7,7 @@ MIT license
 import random
 from typing import Union
 
+from mamba_ssm import Mamba
 import numpy as np
 import torch
 import torch.nn as nn
@@ -14,313 +15,37 @@ import torch.nn.functional as F
 from torch import Tensor
 
 
-class GraphAttentionLayer(nn.Module):
-    def __init__(self, in_dim, out_dim, **kwargs):
+class BiMambaBlock(nn.Module):
+    def __init__(self, d_model, d_state=16, d_conv=4, expand=2):
         super().__init__()
-
-        # attention map
-        self.att_proj = nn.Linear(in_dim, out_dim)
-        self.att_weight = self._init_new_params(out_dim, 1)
-
-        # project
-        self.proj_with_att = nn.Linear(in_dim, out_dim)
-        self.proj_without_att = nn.Linear(in_dim, out_dim)
-
-        # batch norm
-        self.bn = nn.BatchNorm1d(out_dim)
-
-        # dropout for inputs
-        self.input_drop = nn.Dropout(p=0.2)
-
-        # activate
-        self.act = nn.SELU(inplace=True)
-
-        # temperature
-        self.temp = 1.
-        if "temperature" in kwargs:
-            self.temp = kwargs["temperature"]
+        # Forward Mamba
+        self.mamba_fwd = Mamba(
+            d_model=d_model,
+            d_state=d_state,
+            d_conv=d_conv,
+            expand=expand,
+        )
+        # Backward Mamba
+        self.mamba_bwd = Mamba(
+            d_model=d_model,
+            d_state=d_state,
+            d_conv=d_conv,
+            expand=expand,
+        )
+        self.norm = nn.LayerNorm(d_model)
 
     def forward(self, x):
-        '''
-        x   :(#bs, #node, #dim)
-        '''
-        # apply input dropout
-        x = self.input_drop(x)
-
-        # derive attention map
-        att_map = self._derive_att_map(x)
-
-        # projection
-        x = self._project(x, att_map)
-
-        # apply batch norm
-        x = self._apply_BN(x)
-        x = self.act(x)
-        return x
-
-    def _pairwise_mul_nodes(self, x):
-        '''
-        Calculates pairwise multiplication of nodes.
-        - for attention map
-        x           :(#bs, #node, #dim)
-        out_shape   :(#bs, #node, #node, #dim)
-        '''
-
-        nb_nodes = x.size(1)
-        x = x.unsqueeze(2).expand(-1, -1, nb_nodes, -1)
-        x_mirror = x.transpose(1, 2)
-
-        return x * x_mirror
-
-    def _derive_att_map(self, x):
-        '''
-        x           :(#bs, #node, #dim)
-        out_shape   :(#bs, #node, #node, 1)
-        '''
-        att_map = self._pairwise_mul_nodes(x)
-        # size: (#bs, #node, #node, #dim_out)
-        att_map = torch.tanh(self.att_proj(att_map))
-        # size: (#bs, #node, #node, 1)
-        att_map = torch.matmul(att_map, self.att_weight)
-
-        # apply temperature
-        att_map = att_map / self.temp
-
-        att_map = F.softmax(att_map, dim=-2)
-
-        return att_map
-
-    def _project(self, x, att_map):
-        x1 = self.proj_with_att(torch.matmul(att_map.squeeze(-1), x))
-        x2 = self.proj_without_att(x)
-
-        return x1 + x2
-
-    def _apply_BN(self, x):
-        org_size = x.size()
-        x = x.view(-1, org_size[-1])
-        x = self.bn(x)
-        x = x.view(org_size)
-
-        return x
-
-    def _init_new_params(self, *size):
-        out = nn.Parameter(torch.FloatTensor(*size))
-        nn.init.xavier_normal_(out)
-        return out
-
-
-class HtrgGraphAttentionLayer(nn.Module):
-    def __init__(self, in_dim, out_dim, **kwargs):
-        super().__init__()
-
-        self.proj_type1 = nn.Linear(in_dim, in_dim)
-        self.proj_type2 = nn.Linear(in_dim, in_dim)
-
-        # attention map
-        self.att_proj = nn.Linear(in_dim, out_dim)
-        self.att_projM = nn.Linear(in_dim, out_dim)
-
-        self.att_weight11 = self._init_new_params(out_dim, 1)
-        self.att_weight22 = self._init_new_params(out_dim, 1)
-        self.att_weight12 = self._init_new_params(out_dim, 1)
-        self.att_weightM = self._init_new_params(out_dim, 1)
-
-        # project
-        self.proj_with_att = nn.Linear(in_dim, out_dim)
-        self.proj_without_att = nn.Linear(in_dim, out_dim)
-
-        self.proj_with_attM = nn.Linear(in_dim, out_dim)
-        self.proj_without_attM = nn.Linear(in_dim, out_dim)
-
-        # batch norm
-        self.bn = nn.BatchNorm1d(out_dim)
-
-        # dropout for inputs
-        self.input_drop = nn.Dropout(p=0.2)
-
-        # activate
-        self.act = nn.SELU(inplace=True)
-
-        # temperature
-        self.temp = 1.
-        if "temperature" in kwargs:
-            self.temp = kwargs["temperature"]
-
-    def forward(self, x1, x2, master=None):
-        '''
-        x1  :(#bs, #node, #dim)
-        x2  :(#bs, #node, #dim)
-        '''
-        num_type1 = x1.size(1)
-        num_type2 = x2.size(1)
-
-        x1 = self.proj_type1(x1)
-        x2 = self.proj_type2(x2)
-
-        x = torch.cat([x1, x2], dim=1)
-
-        if master is None:
-            master = torch.mean(x, dim=1, keepdim=True)
-
-        # apply input dropout
-        x = self.input_drop(x)
-
-        # derive attention map
-        att_map = self._derive_att_map(x, num_type1, num_type2)
-
-        # directional edge for master node
-        master = self._update_master(x, master)
-
-        # projection
-        x = self._project(x, att_map)
-
-        # apply batch norm
-        x = self._apply_BN(x)
-        x = self.act(x)
-
-        x1 = x.narrow(1, 0, num_type1)
-        x2 = x.narrow(1, num_type1, num_type2)
-
-        return x1, x2, master
-
-    def _update_master(self, x, master):
-
-        att_map = self._derive_att_map_master(x, master)
-        master = self._project_master(x, master, att_map)
-
-        return master
-
-    def _pairwise_mul_nodes(self, x):
-        '''
-        Calculates pairwise multiplication of nodes.
-        - for attention map
-        x           :(#bs, #node, #dim)
-        out_shape   :(#bs, #node, #node, #dim)
-        '''
-
-        nb_nodes = x.size(1)
-        x = x.unsqueeze(2).expand(-1, -1, nb_nodes, -1)
-        x_mirror = x.transpose(1, 2)
-
-        return x * x_mirror
-
-    def _derive_att_map_master(self, x, master):
-        '''
-        x           :(#bs, #node, #dim)
-        out_shape   :(#bs, #node, #node, 1)
-        '''
-        att_map = x * master
-        att_map = torch.tanh(self.att_projM(att_map))
-
-        att_map = torch.matmul(att_map, self.att_weightM)
-
-        # apply temperature
-        att_map = att_map / self.temp
-
-        att_map = F.softmax(att_map, dim=-2)
-
-        return att_map
-
-    def _derive_att_map(self, x, num_type1, num_type2):
-        '''
-        x           :(#bs, #node, #dim)
-        out_shape   :(#bs, #node, #node, 1)
-        '''
-        att_map = self._pairwise_mul_nodes(x)
-        # size: (#bs, #node, #node, #dim_out)
-        att_map = torch.tanh(self.att_proj(att_map))
-        # size: (#bs, #node, #node, 1)
-
-        att_board = torch.zeros_like(att_map[:, :, :, 0]).unsqueeze(-1)
-
-        att_board[:, :num_type1, :num_type1, :] = torch.matmul(
-            att_map[:, :num_type1, :num_type1, :], self.att_weight11)
-        att_board[:, num_type1:, num_type1:, :] = torch.matmul(
-            att_map[:, num_type1:, num_type1:, :], self.att_weight22)
-        att_board[:, :num_type1, num_type1:, :] = torch.matmul(
-            att_map[:, :num_type1, num_type1:, :], self.att_weight12)
-        att_board[:, num_type1:, :num_type1, :] = torch.matmul(
-            att_map[:, num_type1:, :num_type1, :], self.att_weight12)
-
-        att_map = att_board
-
-        # att_map = torch.matmul(att_map, self.att_weight12)
-
-        # apply temperature
-        att_map = att_map / self.temp
-
-        att_map = F.softmax(att_map, dim=-2)
-
-        return att_map
-
-    def _project(self, x, att_map):
-        x1 = self.proj_with_att(torch.matmul(att_map.squeeze(-1), x))
-        x2 = self.proj_without_att(x)
-
-        return x1 + x2
-
-    def _project_master(self, x, master, att_map):
-
-        x1 = self.proj_with_attM(torch.matmul(
-            att_map.squeeze(-1).unsqueeze(1), x))
-        x2 = self.proj_without_attM(master)
-
-        return x1 + x2
-
-    def _apply_BN(self, x):
-        org_size = x.size()
-        x = x.view(-1, org_size[-1])
-        x = self.bn(x)
-        x = x.view(org_size)
-
-        return x
-
-    def _init_new_params(self, *size):
-        out = nn.Parameter(torch.FloatTensor(*size))
-        nn.init.xavier_normal_(out)
-        return out
-
-
-class GraphPool(nn.Module):
-    def __init__(self, k: float, in_dim: int, p: Union[float, int]):
-        super().__init__()
-        self.k = k
-        self.sigmoid = nn.Sigmoid()
-        self.proj = nn.Linear(in_dim, 1)
-        self.drop = nn.Dropout(p=p) if p > 0 else nn.Identity()
-        self.in_dim = in_dim
-
-    def forward(self, h):
-        Z = self.drop(h)
-        weights = self.proj(Z)
-        scores = self.sigmoid(weights)
-        new_h = self.top_k_graph(scores, h, self.k)
-
-        return new_h
-
-    def top_k_graph(self, scores, h, k):
-        """
-        args
-        =====
-        scores: attention-based weights (#bs, #node, 1)
-        h: graph data (#bs, #node, #dim)
-        k: ratio of remaining nodes, (float)
-
-        returns
-        =====
-        h: graph pool applied data (#bs, #node', #dim)
-        """
-        _, n_nodes, n_feat = h.size()
-        n_nodes = max(int(n_nodes * k), 1)
-        _, idx = torch.topk(scores, n_nodes, dim=1)
-        idx = idx.expand(-1, -1, n_feat)
-
-        h = h * scores
-        h = torch.gather(h, 1, idx)
-
-        return h
-
+        # x shape: (Batch, Seq_Len, Dim)
+        x_fwd = self.mamba_fwd(x)
+        
+        # Flip for backward pass to model bidirectional context
+        x_rev = torch.flip(x, [1])
+        x_bwd = self.mamba_bwd(x_rev)
+        x_bwd = torch.flip(x_bwd, [1])
+        
+        # Combine
+        x_out = x_fwd + x_bwd
+        return self.norm(x_out)
 
 class CONV(nn.Module):
     @staticmethod
@@ -472,17 +197,12 @@ class Model(nn.Module):
 
         self.d_args = d_args
         filts = d_args["filts"]
-        gat_dims = d_args["gat_dims"]
-        pool_ratios = d_args["pool_ratios"]
-        temperatures = d_args["temperatures"]
-
+        
+        # --- Frontend (Same as AASIST) ---
         self.conv_time = CONV(out_channels=filts[0],
                               kernel_size=d_args["first_conv"],
                               in_channels=1)
         self.first_bn = nn.BatchNorm2d(num_features=1)
-
-        self.drop = nn.Dropout(0.5, inplace=True)
-        self.drop_way = nn.Dropout(0.2, inplace=True)
         self.selu = nn.SELU(inplace=True)
 
         self.encoder = nn.Sequential(
@@ -493,40 +213,22 @@ class Model(nn.Module):
             nn.Sequential(Residual_block(nb_filts=filts[4])),
             nn.Sequential(Residual_block(nb_filts=filts[4])))
 
-        self.pos_S = nn.Parameter(torch.randn(1, 23, filts[-1][-1]))
-        self.master1 = nn.Parameter(torch.randn(1, 1, gat_dims[0]))
-        self.master2 = nn.Parameter(torch.randn(1, 1, gat_dims[0]))
+        # --- Mamba Backend (Replaces Graph Logic) ---
+        self.backend_dim = filts[-1][-1]
+        
+        # Spectral Mamba (Frequency Sequence)
+        self.pos_S = nn.Parameter(torch.randn(1, 23, self.backend_dim)) 
+        self.mamba_S = BiMambaBlock(d_model=self.backend_dim)
 
-        self.GAT_layer_S = GraphAttentionLayer(filts[-1][-1],
-                                               gat_dims[0],
-                                               temperature=temperatures[0])
-        self.GAT_layer_T = GraphAttentionLayer(filts[-1][-1],
-                                               gat_dims[0],
-                                               temperature=temperatures[1])
-
-        self.HtrgGAT_layer_ST11 = HtrgGraphAttentionLayer(
-            gat_dims[0], gat_dims[1], temperature=temperatures[2])
-        self.HtrgGAT_layer_ST12 = HtrgGraphAttentionLayer(
-            gat_dims[1], gat_dims[1], temperature=temperatures[2])
-
-        self.HtrgGAT_layer_ST21 = HtrgGraphAttentionLayer(
-            gat_dims[0], gat_dims[1], temperature=temperatures[2])
-
-        self.HtrgGAT_layer_ST22 = HtrgGraphAttentionLayer(
-            gat_dims[1], gat_dims[1], temperature=temperatures[2])
-
-        self.pool_S = GraphPool(pool_ratios[0], gat_dims[0], 0.3)
-        self.pool_T = GraphPool(pool_ratios[1], gat_dims[0], 0.3)
-        self.pool_hS1 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
-        self.pool_hT1 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
-
-        self.pool_hS2 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
-        self.pool_hT2 = GraphPool(pool_ratios[2], gat_dims[1], 0.3)
-
-        self.out_layer = nn.Linear(5 * gat_dims[1], 2)
+        # Temporal Mamba (Time Sequence)
+        self.mamba_T = BiMambaBlock(d_model=self.backend_dim)
+        
+        # Pooling and Output
+        self.pool_drop = nn.Dropout(0.3)
+        self.out_layer = nn.Linear(self.backend_dim * 4, 2) # Concatenating Max/Avg from both streams
 
     def forward(self, x, Freq_aug=False):
-
+        # x: (Batch, Samples)
         x = x.unsqueeze(1)
         x = self.conv_time(x, mask=Freq_aug)
         x = x.unsqueeze(dim=1)
@@ -534,74 +236,42 @@ class Model(nn.Module):
         x = self.first_bn(x)
         x = self.selu(x)
 
-        # get embeddings using encoder
-        # (#bs, #filt, #spec, #seq)
+        # Encoder output: (Batch, Channels, Freq, Time)
         e = self.encoder(x)
+        
+        # --- Prepare Spectral Stream ---
+        # Pool over time to get Frequency sequence
+        e_S, _ = torch.max(torch.abs(e), dim=3) # (Batch, Channels, Freq)
+        e_S = e_S.transpose(1, 2) # (Batch, Freq, Channels)
+        
+        # Add positional embedding if shapes match (simple robustness check)
+        if e_S.shape[1] == self.pos_S.shape[1]:
+             e_S = e_S + self.pos_S
+        
+        # Process with Mamba (Spectral)
+        out_S = self.mamba_S(e_S) 
+        
+        # --- Prepare Temporal Stream ---
+        # Pool over freq to get Time sequence
+        e_T, _ = torch.max(torch.abs(e), dim=2) # (Batch, Channels, Time)
+        e_T = e_T.transpose(1, 2) # (Batch, Time, Channels)
+        
+        # Process with Mamba (Temporal)
+        out_T = self.mamba_T(e_T)
+        
+        # --- Readout (Pooling) ---
+        # 1. Max and Avg Pooling for Spectral
+        s_max, _ = torch.max(out_S, dim=1)
+        s_avg = torch.mean(out_S, dim=1)
+        
+        # 2. Max and Avg Pooling for Temporal
+        t_max, _ = torch.max(out_T, dim=1)
+        t_avg = torch.mean(out_T, dim=1)
+        
+        # Combine all features
+        feat = torch.cat([s_max, s_avg, t_max, t_avg], dim=1)
+        
+        feat = self.pool_drop(feat)
+        output = self.out_layer(feat)
 
-        # spectral GAT (GAT-S)
-        e_S, _ = torch.max(torch.abs(e), dim=3)  # max along time
-        e_S = e_S.transpose(1, 2) + self.pos_S
-
-        gat_S = self.GAT_layer_S(e_S)
-        out_S = self.pool_S(gat_S)  # (#bs, #node, #dim)
-
-        # temporal GAT (GAT-T)
-        e_T, _ = torch.max(torch.abs(e), dim=2)  # max along freq
-        e_T = e_T.transpose(1, 2)
-
-        gat_T = self.GAT_layer_T(e_T)
-        out_T = self.pool_T(gat_T)
-
-        # learnable master node
-        master1 = self.master1.expand(x.size(0), -1, -1)
-        master2 = self.master2.expand(x.size(0), -1, -1)
-
-        # inference 1
-        out_T1, out_S1, master1 = self.HtrgGAT_layer_ST11(
-            out_T, out_S, master=self.master1)
-
-        out_S1 = self.pool_hS1(out_S1)
-        out_T1 = self.pool_hT1(out_T1)
-
-        out_T_aug, out_S_aug, master_aug = self.HtrgGAT_layer_ST12(
-            out_T1, out_S1, master=master1)
-        out_T1 = out_T1 + out_T_aug
-        out_S1 = out_S1 + out_S_aug
-        master1 = master1 + master_aug
-
-        # inference 2
-        out_T2, out_S2, master2 = self.HtrgGAT_layer_ST21(
-            out_T, out_S, master=self.master2)
-        out_S2 = self.pool_hS2(out_S2)
-        out_T2 = self.pool_hT2(out_T2)
-
-        out_T_aug, out_S_aug, master_aug = self.HtrgGAT_layer_ST22(
-            out_T2, out_S2, master=master2)
-        out_T2 = out_T2 + out_T_aug
-        out_S2 = out_S2 + out_S_aug
-        master2 = master2 + master_aug
-
-        out_T1 = self.drop_way(out_T1)
-        out_T2 = self.drop_way(out_T2)
-        out_S1 = self.drop_way(out_S1)
-        out_S2 = self.drop_way(out_S2)
-        master1 = self.drop_way(master1)
-        master2 = self.drop_way(master2)
-
-        out_T = torch.max(out_T1, out_T2)
-        out_S = torch.max(out_S1, out_S2)
-        master = torch.max(master1, master2)
-
-        T_max, _ = torch.max(torch.abs(out_T), dim=1)
-        T_avg = torch.mean(out_T, dim=1)
-
-        S_max, _ = torch.max(torch.abs(out_S), dim=1)
-        S_avg = torch.mean(out_S, dim=1)
-
-        last_hidden = torch.cat(
-            [T_max, T_avg, S_max, S_avg, master.squeeze(1)], dim=1)
-
-        last_hidden = self.drop(last_hidden)
-        output = self.out_layer(last_hidden)
-
-        return last_hidden, output
+        return feat, output
